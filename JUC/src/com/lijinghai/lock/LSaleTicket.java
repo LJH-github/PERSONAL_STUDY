0c51bb3ca7d44ca35cdb0d7c95e05ccf844a393b
ljh <1358672989@qq.com>
2021-07-18 14:10:45 +0800

    small modify

--- JUC/src/com/lijinghai/lock/LSaleTicket.java
+++ JUC/src/com/lijinghai/lock/LSaleTicket.java
@@ -2,13 +2,26 @@ package com.lijinghai.lock;
 
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 涉及公平锁和非公平锁
+ *      ==>公平锁
+ *          ：可能造成线程会被饿死；效率高；
+ *      ==>非公平锁
+ *          ：雨露均沾；效率相对较低；
+ *
+ * private final ReentrantLock reentrantLock = new ReentrantLock();  无参构造，默认是非公平锁
+ * private final ReentrantLock reentrantLock = new ReentrantLock(true); 有参构造，公平锁
+ *
+ */
+
 class LTicket {
 
     //票数
     private int number = 30;
 
     //创建可重入锁
-    private final ReentrantLock reentrantLock = new ReentrantLock();
+//    private final ReentrantLock reentrantLock = new ReentrantLock();  //非公平锁
+    private final ReentrantLock reentrantLock = new ReentrantLock(true);  //非公平锁
 
     //卖票方法
     public void sale() {
